1cf7ec6737dad4824124c57d46cdb06ffaca65df
Michal Halabica <m.halabica@gmail.com>
2022-04-28 20:30:17 +0200

    Deploy scripts

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,3 @@
-#!/bin/sh
-
+git push;
 docker build -t registry.gitlab.com/grillbot/grillbot-docs .
 docker push registry.gitlab.com/grillbot/grillbot-docs
--- docker.ps1
+++
@@ -1,2 +0,0 @@
-docker build -t registry.gitlab.com/grillbot/grillbot-docs .
-docker push registry.gitlab.com/grillbot/grillbot-docs
