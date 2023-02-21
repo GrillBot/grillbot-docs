d802d8d758046e01b5724aa90c32e0ac910a4a19
Michal Halabica <m.halabica@gmail.com>
2023-02-21 22:31:58 +0100

    Fixed flake8

--- src/source/dotnet.py
+++ src/source/dotnet.py
@@ -15,8 +15,8 @@ class DotnetDependencyList:
             "https://raw.githubusercontent.com/GrillBot/grillbot/master/src/GrillBot.Data/GrillBot.Data.csproj",
             "https://raw.githubusercontent.com/GrillBot/grillbot/master/src/GrillBot.Database/GrillBot.Database.csproj",
             "https://raw.githubusercontent.com/GrillBot/grillbot/master/src/GrillBot.Tests/GrillBot.Tests.csproj",
-            "https://raw.githubusercontent.com/GrillBot/rubbergodService/master/RubbergodService/RubbergodService.csproj", # noqa: E501
-            "https://raw.githubusercontent.com/GrillBot/rubbergodService/master/RubbergodService.Data/RubbergodService.Data.csproj" # noqa: E501
+            "https://raw.githubusercontent.com/GrillBot/rubbergodService/master/RubbergodService/RubbergodService.csproj",  # noqa: E501
+            "https://raw.githubusercontent.com/GrillBot/rubbergodService/master/RubbergodService.Data/RubbergodService.Data.csproj"  # noqa: E501
         ]
         self.cache = cache
         self.cache_key = "dotnet_dependency_list"
